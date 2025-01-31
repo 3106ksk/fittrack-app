fa21928d3fcb8eab684d7d002c460754703b0ab2
satoru <ksk1889satoru18@i.softbank.jp>
2025-01-31 10:03:44 +0000

    hashedpass-and-createmethod

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,46 +2,36 @@ const router = require("express").Router();
 const { body, query, validationResult } = require('express-validator');
 const { User } = require('../models');
 const bcrypt = require('bcrypt');
-
-
+const JWT = require("jsonwebtoken")
 
 router.post("/register",
   [body("username").notEmpty().withMessage("Username is required"),
   body("email").isEmail().withMessage("Invalid email format"),
-  body("passward").isLength({ min: 6 }).withMessage("Passward is minimum 6characters")
+  body("password").isLength({ min: 6 }).withMessage("password is minimum 6characters")
   ], async (req, res) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const passward = req.body.passward;
+    const { username, email, password } = req.body;
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     try {
-      const user = await User.findOne({
-        where: {
-          username: username,
-          email: email,
-        }
-      });
-
-      if (user) {
-        return res.status(400).json([
-          {
-            message: "this user is already existed",
-          }
-        ])
+      const existingUser = await User.findOne({ where: { username } });
+      if (existingUser) {
+        return res.status(400).json([{ message: "This username is already taken" }]);
       }
-
+      let hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await User.create({ username, email, password: hashedPassword });
+      return res.status(201).json(newUser);
     } catch (error) {
       console.error("Error finding user:", error);
       return res.status(500).json({ message: "Internal server error" });
     }
-
-
-
   });
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body
+})
 
 
 module.exports = router;
