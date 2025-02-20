59cd1227c1a109cf99fe37ae71467a7c9dfde284
satoru <ksk1889satoru18@i.softbank.jp>
2025-02-20 16:28:42 +0000

    authentication-error-resoloved

--- backend/middleware/checkJWT.js
+++ backend/middleware/checkJWT.js
@@ -2,8 +2,9 @@ const { expressjwt: jwt } = require("express-jwt");
 require('dotenv').config();
 
 const authMiddleware = jwt({
-  secret: "23d3b7c1dfc3eac7307ca97b5e345ff526e8534ae10bf1bb66f292291099910e",
-  algorithms: ['HS256']
+  secret: process.env.JWT_SECRET_KEY,
+  algorithms: ['HS256'],
+  requestProperty: 'user'
 });
 
 const usingtken = process.env.JWT_SECRET_KEY;
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,14 +1,13 @@
 const router = require("express").Router();
-const { User } = require("../models/Users");
+const { User } = require("../models");
 const authMiddleware = require("../middleware/checkJWT");
 
 router.get('/userworkout', authMiddleware, async (req, res) => {
-  console.log("Request Headers:", req.headers);
+
   if (!req.user) {
     return res.status(401).json({ error: "Unauthorized - No user data in request" });
   }
   const userId = req.user.id;
-  console.log("Extracted userId:", userId);
 
   try {
     const user = await User.findByPk(userId);
